1bfa7e0c05386c163313d6060f680fc1b0798d25
sara1142 <saraf1142@hotmail.com>
2018-11-11 13:55:39 +0300

    PsWorldApp

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  devise_for :admins
+  mount Ckeditor::Engine => '/ckeditor'
+  root to: "posts#index"
+
+  resources :posts
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
